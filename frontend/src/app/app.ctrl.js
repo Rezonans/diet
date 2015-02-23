6dc8f900e53b677e9f7ca26c34f1912c169bf088
Rodik <i6@bk.ru>
2015-02-23 23:00:36 +0200

    Calories stat example

--- frontend/src/app/app.ctrl.js
+++ frontend/src/app/app.ctrl.js
@@ -2,7 +2,12 @@
 
 /* Controllers */
 
-angular.module('app').controller('AppCtrl', function($scope, $auth) {
+angular.module('app').controller('AppCtrl', function($scope, $auth, $http) {
+
+  $http.get('api/calories').then(function(calories) {
+    $scope.labels = _.pluck(calories.data, "date_only");
+    $scope.data = [_.pluck(calories.data, "calories_sum")];
+  });
 
   $scope.logout = logout;
 
--- frontend/src/module.js
+++ frontend/src/module.js
@@ -11,5 +11,6 @@ angular.module('app', [
   'ui.router',
   'permission',
   'satellizer',
-  'restangular'
+  'restangular',
+  'chart.js'
 ]);
